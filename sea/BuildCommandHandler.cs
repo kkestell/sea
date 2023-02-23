b7e3e090dea790490dcf154503635a8175b290c6
Kyle Kestell <kyle@kestell.org>
2023-02-23 10:59:26 -0600

    Sync

--- sea/BuildCommandHandler.cs
+++ sea/BuildCommandHandler.cs
@@ -1,4 +1,6 @@
-﻿namespace Sea;
+﻿using Spectre.Console;
+
+namespace Sea;
 
 internal class BuildCommandHandler
 {
@@ -13,28 +15,39 @@ internal class BuildCommandHandler
     {
         try
         {
-            var buildOptions = new BuildOptions(command);
-
-            if (buildOptions.Verbose)
-                Logger.Log(buildOptions.ToString());
-
-            if (!Directory.Exists(buildOptions.OutputDirectory.FullName))
-                Directory.CreateDirectory(buildOptions.OutputDirectory.FullName);
-
-            var ilGenerator = new BytecodeGenerator(new BytecodeGeneratorOptions(buildOptions));
-            var ilFile = ilGenerator.Emit(buildOptions.OutputDirectory);
-
-            var nativeObjectGenerator = new NativeObjectGenerator(ilFile, new NativeObjectGeneratorOptions(buildOptions));
-            var objectFile = nativeObjectGenerator.Emit(buildOptions.OutputDirectory);
-
-            var linker = new Linker(objectFile, new LinkerOptions(buildOptions));
-            var executable = linker.Emit(buildOptions.OutputDirectory);
-
-            if (buildOptions.Strip && Platform.OperatingSystem != OperatingSystem.Windows)
+            AnsiConsole.Status().Spinner(Spinner.Known.Dots).Start("Generating IL", ctx => 
             {
-                var stripper = new Stripper(new StripperOptions(buildOptions));
-                stripper.Run(executable);
-            }
+                ctx.SpinnerStyle(Style.Parse("purple"));
+
+                var buildOptions = new BuildOptions(command);
+
+                if (buildOptions.Verbose)
+                    Logger.Log(buildOptions.ToString());
+
+                if (!Directory.Exists(buildOptions.OutputDirectory.FullName))
+                    Directory.CreateDirectory(buildOptions.OutputDirectory.FullName);
+
+                var ilGenerator = new BytecodeGenerator(new BytecodeGeneratorOptions(buildOptions));
+                var ilFile = ilGenerator.Emit(buildOptions.OutputDirectory);
+
+                ctx.Status("Compiling IL");
+
+                var nativeObjectGenerator = new NativeObjectGenerator(ilFile, new NativeObjectGeneratorOptions(buildOptions));
+                var objectFile = nativeObjectGenerator.Emit(buildOptions.OutputDirectory);
+
+                ctx.Status("Linking");
+
+                var linker = new Linker(objectFile, new LinkerOptions(buildOptions));
+                var executable = linker.Emit(buildOptions.OutputDirectory);
+
+                if (buildOptions.Strip && Platform.OperatingSystem != OperatingSystem.Windows)
+                {
+                    ctx.Status("Stripping");
+
+                    var stripper = new Stripper(new StripperOptions(buildOptions));
+                    stripper.Run(executable);
+                }
+            });
 
             return 0;
         }
