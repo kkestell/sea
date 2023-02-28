5cf05b637a67dd47c7398ebc440dbe2ac862a0a8
Kyle Kestell <kyle@kestell.org>
2023-02-28 08:11:49 -0600

    Sync

---
+++ examples/hello/README.md
@@ -0,0 +1,17 @@
+# Hello, World!
+
+## Visual Studio Code
+
+Open this directory in Visual Studio Code. Install the official C# extension from Microsoft (`ms-dotnettools.csharp`). Start debugging.
+
+## Command Line
+
+```console
+$ sea build hello.cs
+Build succeeded.
+```
+
+```console
+$ ./hello
+Hello, World!
+```
--- scripts/3-test.sh
+++ scripts/3-test.sh
@@ -7,12 +7,12 @@ source "$SCRIPT_PATH/vars.sh"
 DOTNET_ROOT="$SDK_PATH"
 PATH="$DOTNET_ROOT:$BUILD_PATH:$PATH"
 
-cat <<EOT > hello.cs
+cat <<EOT > "$ROOT_PATH"/examples/hello/hello.cs
 Console.WriteLine("Hello, Sea!");
 EOT
 
 echo 'Testing build command (small)...'
-sea build hello.cs --optimize:small --reflection:false --stacktrace:false --strip --verbosity:diagnostic
+sea build "$ROOT_PATH"/examples/hello/hello.cs --optimize:small --reflection:false --stacktrace:false --strip --verbosity:diagnostic
 # ./hello
 # echo $(du -sh hello)
 
@@ -24,4 +24,7 @@ sea build hello.cs --optimize:small --reflection:false --stacktrace:false --stri
 # echo 'Testing run command...'
 # sea run hello.cs
 
-rm hello*
+rm "$ROOT_PATH"/examples/hello/hello.dll
+rm "$ROOT_PATH"/examples/hello/hello.ilc.rsp
+rm "$ROOT_PATH"/examples/hello/hello.o
+rm "$ROOT_PATH"/examples/hello/hello
